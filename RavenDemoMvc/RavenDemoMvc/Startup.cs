6ff29eeacb6d689a09baeb508d4a450f78381761
Nick Josevski <nick@josevski.com>
2014-02-04 21:31:03 +1100

    File->New Project

---
+++ RavenDemoMvc/RavenDemoMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RavenDemoMvc.Startup))]
+namespace RavenDemoMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
